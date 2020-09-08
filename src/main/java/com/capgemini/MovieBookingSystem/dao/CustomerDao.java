72c0d857b1bfd5e39cd9fda171941f5686210a03
Shreesh Agrawal <shreesh1234@gmail.com>
2020-09-08 13:38:14 +0530

    Created remaining class structure

--- src/main/java/com/capgemini/MovieBookingSystem/dao/CustomerDao.java
+++ src/main/java/com/capgemini/MovieBookingSystem/dao/CustomerDao.java
@@ -3,14 +3,14 @@ package com.capgemini.MovieBookingSystem.dao;
 import java.util.Optional;
 
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.capgemini.MovieBookingSystem.entities.Customer;
 
 @Repository
 @EnableScan
-public interface CustomerDao extends CrudRepository<Customer, String> {
+public interface CustomerDao extends PagingAndSortingRepository<Customer, String> {
 	
 	Optional<Customer> findById(String id);
 	
